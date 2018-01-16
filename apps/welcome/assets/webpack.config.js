213393e7a7c566f5898e902e77cdfce237126288
Lenz Gschwendtner <lenz@springtimesoft.com>
2018-01-16 13:06:52 +1300

    still got webpack build issues

--- apps/welcome/assets/webpack.config.js
+++ apps/welcome/assets/webpack.config.js
@@ -1,14 +1,17 @@
 var path = require('path')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var CopyWebpackPlugin = require('copy-webpack-plugin')
 var webpack = require('webpack')
 var env = process.env.MIX_ENV || 'dev'
-var isProduction = (env === 'prod')
 
+const RewriteImportPlugin = require("less-plugin-rewrite-import");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ROOT_DIR = path.resolve(__dirname);
+const SRC_DIR = path.resolve(__dirname, '../priv/static/');
+const BUILD_DIR = path.resolve(__dirname, 'build');
+const NODE_MODULES_DIR = path.resolve(__dirname, 'node_modules');
 
 module.exports = {
   entry: {
-    'app': [__dirname + '/js/app.js', __dirname + '/css/app.less']
+    'app': __dirname + '/js/app.js'
   },
   output: {
     path: path.resolve(__dirname, '../priv/static/'),
@@ -16,25 +19,33 @@ module.exports = {
   },
   devtool: 'source-map',
   resolve: {
-    extensions: ['.js', '.jsx']
+    // extensions: ['.js', '.jsx'],
+    modules: [path.resolve(__dirname), 'node_modules'],
+    alias: {
+      jquery: path.resolve(__dirname, 'node_modules/jquery/src/jquery')
+    }
   },
   module: {
-    rules: [
+    loaders: [
       {
-        test: /\.less$/,
-        include: /css/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {loader: 'css-loader'},
-            {
-              loader: 'less-loader',
-              options: {
-                sourceComments: !isProduction
-              }
+        test: /\.(less|config)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              paths: [ROOT_DIR, NODE_MODULES_DIR],
+              plugins: [
+                new RewriteImportPlugin({
+                  paths: {
+                    '../../theme.config':  __dirname + '/css/theme.config',
+                  },
+                }),
+              ],
             }
-          ]
-        })
+          }
+        ]
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -59,46 +70,28 @@ module.exports = {
         }
       },
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader"
-      }
+        loader: 'babel-loader',
+        query: {presets: ['es2015']}
+      },
+      { 
+		test: /\.eex/, 
+		loader: 'ejs-loader' 
+	  }
     ]
   },
   plugins: [
-    new CopyWebpackPlugin([{ from: './static' }]),
-    new ExtractTextPlugin('css/app.css'),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false,
-      options: {
-        postcss: [
-          ()=>require("autoprefixer"),
-        ]
-      }
-    }),
+	new HtmlWebpackPlugin({
+      template: '../lib/welcome_web/templates/layout/app.html.eex',
+	  filename: 'app.html.eex',
+	  inject: 'body', // inject scripts before closing body tag
+      env: process.env
+	}),
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
       "window.jQuery": "jquery"
-    // }),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   sourceMap: true,
-    //   compress: {
-    //     warnings: false,
-    //     screw_ie8: true,
-    //     conditionals: true,
-    //     unused: true,
-    //     comparisons: true,
-    //     sequences: true,
-    //     dead_code: true,
-    //     evaluate: true,
-    //     if_return: true,
-    //     join_vars: true,
-    //   },
-    //   output: {
-    //     comments: false
-    //   },
     })
   ]
 };
