f09979c68b428a7b8e21cce29948ec9f1c7ca6a6
Lenz Gschwendtner <lenz@springtimesoft.com>
2018-01-22 18:24:43 +1300

    stubbed out the resto of the API

--- apps/registry/lib/registry.ex
+++ apps/registry/lib/registry.ex
@@ -4,11 +4,11 @@ defmodule ENoTime.Registry do
   """
 
   @doc """
-  iRegistering a worker is simple:
+  Registering a worker is simple:
 
   ## Examples
 
-      iex> ENoTime.Registry.add(name)
+      iex> ENoTime.Registry.add("foo")
       :ok
 
   """
@@ -18,4 +18,38 @@ defmodule ENoTime.Registry do
 	  {:error, _response} -> :error
 	end
   end
+
+  @doc """
+  Looking up a worker is simple as well:
+
+  ## Examples
+
+      iex> ENoTime.Registry.find("foo")
+	  %{"ID" => "foo", "Address" => "", "CreateIndex" => 0, "EnableTagOverride" => false, "ModifyIndex" => 0, "Port" => 0, "Service" => "foo", "Tags" => []}
+
+  """
+  def find(name) do
+    case Consul.Agent.services() do
+	  {:ok, response} -> 
+		response.body[name]
+	  {:error, _response} -> :error
+	end
+  end
+
+  @doc """
+  Removing a worker from the registry is also simple:
+
+  ## Examples
+
+      iex> ENoTime.Registry.del("foo")
+      :ok
+
+  """
+  def del(name) do
+    case Consul.Agent.Service.deregister(name) do
+	  {:ok, _response} -> :ok
+	  {:error, _response} -> :error
+	end
+  end
+
 end
